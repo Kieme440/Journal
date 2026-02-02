c9e55a4dad12c4add299dca708772d835884bc8a
Kieme440 <Kieme440@gmail.com>
2026-02-02 12:56:39 +0100

    front

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from "react-router";
+import Homepage from "./Pages/Homepage";
+import CreatePage from "./Pages/CreatePage";
+import JournalInfoPage from "./Pages/JournalInfoPage";
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/journal/:id" element={<JournalInfoPage />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ Frontend/src/Pages/JournalInfoPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const JournalInfoPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+};
+
+export default JournalInfoPage;
