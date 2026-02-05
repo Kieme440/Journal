e5c6506848bfa7fa5cfbe4b2e7d93fd09802d302
Kieme440 <Kieme440@gmail.com>
2026-02-05 08:38:55 +0100

    front

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Route, Routes } from "react-router";
-import Homepage from "./Pages/Homepage";
-import CreatePage from "./Pages/CreatePage";
-import JournalInfoPage from "./Pages/JournalInfoPage";
+import { Route, Routes} from "react-router-dom";
+import HomePage from './pages/HomePage';
+import CreatePage from './pages/CreatePage';
+import JournalInfoPage from './pages/JournalInfoPage';
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
         <Route path="/journal/:id" element={<JournalInfoPage />} />
       </Routes>
